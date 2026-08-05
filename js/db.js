ca114cf6002659a932e2d0ce14ba52943b018c69
koji <blackknight.hbs>
2026-08-05 12:08:56 +0900

    08/05/2026

---
+++ js/db.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+let tab=document.getElementById("db")
+let htm=
+`
+<table class="table align-middle text-center">
+<tbody>
+<tr>
+<td>
+</td>
+
+<td>
+</td>
+
+</tr>
+</tbody>
+</table>
+`
+tab.innerHTML=htm
+})
---
+++ js/nav.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+let navContainer = document.getElementById('nav');
+
+let navbarHTML = `
+<div class=container-fluid>
+<ul class="bg-info navbar-nav ms-auto mb-2 mb-lg-0 d-flex flex-row gap-3">
+
+<li class="nav-item">
+<a class="navbar-brand d-flex align-items-center me-auto" href="index.html">
+<img src="img/g2.png" alt="Blue Beard" class="d-inline-block align-text-top me-2" style="max-height: 40px;">
+<span class="fw-bold">bash</span>
+</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link text-white fw-medium" href="bash.htm">bash</a>
+</li>
+<li class="nav-item">
+<a class="nav-link text-white fw-medium" href="cmd.htm">cmd</a>
+</li>
+<li class="nav-item">
+<a class="nav-link text-white fw-medium" href="db.htm">db</a>
+</li>
+
+</ul>
+</div>
+`;
+
+if (navContainer) {navContainer.innerHTML = navbarHTML;}
+});

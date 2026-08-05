d311a8f48d3dc72dc6cf6470596eb69e895ae2a3
koji <blackknight.hbs>
2026-08-05 14:31:08 +0900

    08/05/2026

--- js/Bdb.js
+++ js/Bdb.js
@@ -1,28 +1,37 @@
 async function rd() {
-  const initSqlJs = window.initSqlJs
+  const initSqlJs = window.initSqlJs;
   const SQL = await initSqlJs({
     locateFile: file => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/${file}`
-  })
+  });
 
-  const response = await fetch('/db/bash.db')
-  const buffer = await response.arrayBuffer()
+  const response = await fetch('/db/bash.db');
+  const buffer = await response.arrayBuffer();
 
-  const db = new SQL.Database(new Uint8Array(buffer))
+  const db = new SQL.Database(new Uint8Array(buffer));
 
-  const stmt = db.prepare("SELECT * FROM bash WHERE term=?")
-  stmt.get("bash")
+  const stmt = db.prepare("SELECT * FROM bash WHERE term=?");
+  // Note: stmt.get("bash") executes the query for the first row,
+  // but to loop through all results with stmt.step(), use bind instead:
+  stmt.bind(["bash"]);
 
-  const res = []
+  const res = [];
   while (stmt.step()) {
-    const row = stmt.getAsObject()
-    res.push(row)
+    const row = stmt.getAsObject();
+    res.push(row);
   }
 
-  stmt.free()
-  db.close()
+  stmt.free();
+  db.close();
 
-  console.log('result:', res)
-  return res
+  console.log('result:', res);
+
+  // Return the result to element with id 'Bdb'
+  const targetElement = document.getElementById('Bdb');
+  if (targetElement) {
+    targetElement.textContent = JSON.stringify(res, null, 2);
+  }
+
+  return res;
 }
 
-rd()
+rd();

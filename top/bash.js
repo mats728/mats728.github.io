069daff1fd5f09eb120d30f4b8c89523aa07dd1f
koji <blackknight.hbs>
2026-08-05 15:21:17 +0900

    08/05/2026

---
+++ top/bash.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+let tab=document.getElementById("bash")
+let htm=
+`
+<div class="bg-info text-white">bash</div>
+<table class="table align-middle text-center">
+<tbody>
+<tr>
+<td>
+</td>
+
+<td>
+</td>
+
+</tr>
+</tbody>
+</table>
+`
+tab.innerHTML=htm
+})
